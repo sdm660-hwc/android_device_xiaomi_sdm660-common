5140c0e85ef65f33e9c6212d9f2d94b48f05fadc
Mohammad Hasan Keramat J <ikeramat@protonmail.com>
2022-08-15 10:26:32 +0430

    sdm660-common: Update display blobs from S62Pro
    Change-Id: I39b4e5b38e93dbf09100a333b08dd40e753b7a85

--- sdm660.mk
+++ sdm660.mk
@@ -148,6 +148,7 @@ PRODUCT_PACKAGES += \
     gralloc.sdm660 \
     hwcomposer.qcom \
     libdisplayconfig \
+    libgralloc.qti \
     libtinyxml \
     libqdMetaData \
     libqdMetaData.system \
@@ -156,13 +157,16 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.frameworks.displayservice@1.0_32 \
     android.frameworks.displayservice@1.0.vendor \
-    android.hardware.graphics.allocator@2.0-impl:64 \
-    android.hardware.graphics.allocator@2.0-service \
-    android.hardware.graphics.mapper@2.0-impl-2.1 \
+    android.hardware.graphics.mapper@3.0-impl-qti-display \
+    android.hardware.graphics.mapper@4.0-impl-qti-display \
     android.hardware.graphics.composer@2.1-service \
     android.frameworks.displayservice@1.0 \
-    vendor.display.config@1.1 \
-    vendor.qti.hardware.memtrack-service
+    vendor.display.config@2.0 \
+    vendor.qti.hardware.memtrack-service \
+    vendor.qti.hardware.display.allocator-service \
+    vendor.qti.hardware.display.mapper@1.0.vendor \
+    vendor.qti.hardware.display.mapper@1.1.vendor \
+    vendor.qti.hardware.display.mapper@2.0.vendor
 
 # Doze
 PRODUCT_PACKAGES += \
